3c9e60c9188f295d72423859cbc6fcc966383b48
kovarike <danilokovarike.com>
2024-09-04 11:27:09 -0300

    config vercel deploy

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 <!-- No seu repositório GitHub, vá para Settings > Secrets and variables > Actions.
 Clique em New repository secret e adicione um novo segredo com um nome (por exemplo, MY_PAT) e cole o token. -->
+
+### Passos Adicionais
+
+1. **Testar Localmente**: Certifique-se de que sua aplicação funciona conforme esperado localmente com `npm run build` e `npm start` para garantir que não há problemas antes de fazer o deploy.
+
+2. **Integrar com Vercel**:
+   - **Conectar Repositório**: Conecte seu repositório GitHub à Vercel.
+   - **Configurações do Projeto**: Na Vercel, você pode ajustar configurações específicas do projeto se necessário, mas normalmente as configurações padrão funcionam bem.
+
+3. **Deploy Contínuo**:
+   - O fluxo de trabalho do GitHub Actions que forneci anteriormente cuidará do deploy automático sempre que houver um push para a branch `main`.
+
+Ao seguir essas diretrizes, você garante que sua aplicação Next.js utilize as capacidades completas da Vercel e aproveite os recursos de renderização do lado do servidor e outras funcionalidades avançadas que a plataforma oferece.
--- apps/algorithms/next.config.mjs
+++ apps/algorithms/next.config.mjs
@@ -1,41 +1,3 @@
-// /** @type {import("next").NextConfig} */
-// const config = {
-//   poweredByHeader: false,
-//   reactStrictMode: true,
-//   transpilePackages: [], // Remover se não for necessário
-//   basePath: '/Algorithms', // Ajustar se necessário
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "**",
-//       },
-//     ],
-//   },
-//   async redirects() {
-//     return [
-//       {
-//         source: "/",
-//         destination: "/profile",
-//         permanent: true,
-//       },
-//       {
-//         source: "/admin",
-//         destination: "/profile",
-//         permanent: true,
-//       },
-//     ];
-//   },
-// };
-
-// export default config;
-
 /** @type {import("next").NextConfig} */
 const config = {
   webpack: (config, { isServer }) => {
@@ -57,11 +19,10 @@ const config = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "example.com", // Substitua por domínios específicos se possível
+        hostname: "**", // Permitir todos os domínios para imagens
       },
     ],
   },
-  output: 'export',
   async redirects() {
     return [
       {
